e9b5be9ae3d7f4048f3be748881d6ccb7b147d6a
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-08-25 15:24:17 -0400

    Add page to test CameraView control

---
+++ BigIslandBarcode/CameraPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace BigIslandBarcode
+{
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+
+        private void cameraView_FrameReady(object sender, ZXing.Net.Maui.CameraFrameBufferEventArgs e)
+        {
+            string t = "";
+        }
+    }
+}
