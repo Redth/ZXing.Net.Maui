5da0fbd3cd006d8ea939f77d8b1bb8c9a8a359e9
Marc PERROT <marc.perrot@dopnet.fr>
2025-03-08 08:59:07 +0100

    improvement for App

--- BigIslandBarcode/App.xaml.cs
+++ BigIslandBarcode/App.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
 using Application = Microsoft.Maui.Controls.Application;
+
+#nullable enable
 
 namespace BigIslandBarcode
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
 
-			MainPage = new MainPage();
-		}
-	}
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
 }
