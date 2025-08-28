aedf4669a6c17313e5cc4419f1b68d09fc6d3d1c
Marc PERROT <marc.perrot@dopnet.fr>
2025-08-28 12:22:34 +0200

    FrameAnalyzer does not need to be public

--- ZXing.Net.MAUI/Platforms/Android/FrameAnalyzer.cs
+++ ZXing.Net.MAUI/Platforms/Android/FrameAnalyzer.cs
@@ -1,46 +1,47 @@
-﻿using AndroidX.Camera.Core;
+﻿using System;
+using System.Diagnostics;
+
+using AndroidX.Camera.Core;
+
 using Java.Nio;
 
 using Microsoft.Maui.Graphics;
 
-using System;
-using System.Diagnostics;
-
 namespace ZXing.Net.Maui
 {
-	public class FrameAnalyzer : Java.Lang.Object, ImageAnalysis.IAnalyzer
-	{
-		readonly Action<ByteBuffer, Size> frameCallback;
+    internal class FrameAnalyzer : Java.Lang.Object, ImageAnalysis.IAnalyzer
+    {
+        readonly Action<ByteBuffer, Size> frameCallback;
 
         // See:
         // https://github.com/dotnet/android-libraries/issues/767
         // https://github.com/dotnet/android/pull/9656
         public Android.Util.Size DefaultTargetResolution => null;
 
-		public FrameAnalyzer(Action<ByteBuffer, Size> callback)
-		{
-			frameCallback = callback;
-		}
+        public FrameAnalyzer(Action<ByteBuffer, Size> callback)
+        {
+            frameCallback = callback;
+        }
 
-		public void Analyze(IImageProxy image)
-		{
-			try
-			{
-				var buffer = image.GetPlanes()[0].Buffer;
+        public void Analyze(IImageProxy image)
+        {
+            try
+            {
+                var buffer = image.GetPlanes()[0].Buffer;
 
-				var s = new Size(image.Width, image.Height);
+                var s = new Size(image.Width, image.Height);
 
-				frameCallback?.Invoke(buffer, s);
+                frameCallback?.Invoke(buffer, s);
 
-			}
-			catch (Exception e)
-			{
-				Debug.WriteLine(e);
-			}
-			finally
-			{
-				image.Close();
-			}
-		}
-	}
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            finally
+            {
+                image.Close();
+            }
+        }
+    }
 }
